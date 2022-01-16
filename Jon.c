850b1c8ce5bd4c750549b9c977ffa1b0c35bc7dd
Pamela Rod <akahime2014@gmail.com>
2022-01-16 02:12:08 -0300

    version_1

---
+++ Jon.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void main(){
+
+  char jon[7]= "Te amo"; 
+    //declarei uma variável chamada Jon
+    //que contem uma frase
+
+
+    printf("Eu disse que %s\n",jon); 
+    //imprime na tela o que está dentro da variável
+   
+}
+
+
+
+
+
+
+
+//char curso[14] = “C Progressivo”; conta o numero de characteres  e o 0 dentro de []
+//tbm pode ser char curso[] = “C Progressivo”;
+//No print para imprimir a String de dentro da variável usa %s
